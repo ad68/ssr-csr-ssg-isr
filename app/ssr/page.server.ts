bf512bb4634b4d10892379ec8170c78b9db2c1f8
ad68 <47919905+ad68@users.noreply.github.com>
2024-12-30 18:18:58 +0330

    .

--- app/ssr/page.server.ts
+++ app/ssr/page.server.ts
@@ -1,17 +1,15 @@
 "use server";
-
 import axios from "axios";
-
-type PostType = {
+/* type PostType = {
   createdAt: string;
   name: string;
   avatar: string;
   id: string;
-};
+}; */
 
-export const getPosts = async (): Promise<PostType[]> => {
+export const getPosts = async () => {
   try {
-    const response = await axios.get<PostType[]>("https://677265b3ee76b92dd49226c3.mockapi.io/api/v1/posts/my-posts");
+    const response = await axios.get("https://677265b3ee76b92dd49226c3.mockapi.io/api/v1/posts/my-posts");
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
