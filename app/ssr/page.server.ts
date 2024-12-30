319890bbe6cc93935a4fbc26903ec98792f74692
ad68 <47919905+ad68@users.noreply.github.com>
2024-12-30 18:27:44 +0330

    .

--- app/ssr/page.server.ts
+++ app/ssr/page.server.ts
@@ -9,7 +9,13 @@ import axios from "axios";
 
 export const getPosts = async () => {
   try {
-    const response = await axios.get("https://677265b3ee76b92dd49226c3.mockapi.io/api/v1/posts/my-posts");
+    const response = await axios.get("https://677265b3ee76b92dd49226c3.mockapi.io/api/v1/posts/my-posts", {
+      headers: {
+        "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
+        Pragma: "no-cache",
+      },
+      timeout: 5000, // زمان انتظار اختیاری
+    });
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
